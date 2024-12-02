0baf6a3953faa30ac894aae9b2d3809256d43fa0
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2024-12-02 22:57:44 +0530

    test: Add test case for single number

--- src/string-calculator.test.ts
+++ src/string-calculator.test.ts
@@ -5,7 +5,12 @@ describe("String Calculator", () => {
   beforeEach(() => {
     calculator = new StringCalculator();
   });
+
   it("should return 0 for empty string", () => {
     expect(calculator.add("")).toBe(0);
   });
+
+  it("should return the number when single number is passed", () => {
+    expect(calculator.add("1")).toBe(1);
+  })
 });
