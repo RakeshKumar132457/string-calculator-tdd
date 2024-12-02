b48064b80d722002b5bc3a9eb3ddb6145d3172aa
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2024-12-02 22:49:52 +0530

    Initial project setup with TypeScript and Jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testMatch: ["**/*.test.ts"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
---
+++ src/string-calculator.test.ts
@@ -0,0 +1,1 @@
+import { StringCalculator } from "./string-calculator";
