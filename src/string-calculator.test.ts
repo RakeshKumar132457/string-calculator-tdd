9bb2b17d03c1f798f9b3349c49fc0abff811501b
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2024-12-03 00:04:15 +0530

    test: Add test case for multiple delimiters

--- src/string-calculator.test.ts
+++ src/string-calculator.test.ts
@@ -47,4 +47,8 @@ describe("String Calculator", () => {
   it("should support delimiters of any length", () => {
     expect(calculator.add("//[***]\n1***2***3")).toBe(6);
   });
+
+  it("should support multiple delimiters", () => {
+    expect(calculator.add("//[*][%]\n1*2%3")).toBe(6);
+  });
 });
